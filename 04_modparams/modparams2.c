49282f733933e1bf490b115b220050986a108f48
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-08-05 18:31:01 +0530

    add work to do :-)

--- 04_modparams/modparams2.c
+++ 04_modparams/modparams2.c
@@ -7,13 +7,22 @@
 
 MODULE_LICENSE("Dual MIT/GPL");
 
+#if LINUX_VERSION_CODE > KERNEL_VERSION(7, 1, 0)
 static u64 gvar64 = 42;
+#endif
 
 /*
- * 1. Make the global gvar declared above into a module param named 'val1'.
- * 2. Allow it to be passed and set at both 
+ * 1. There's a deliberate bug with the '#if LINUX_VERSION_CODE ...' line; fix it
+ *    so that the variable gvar64 has the value 42 for all kernel versions > 5.0.0
+ *    and the value 21 for all other kernels. Print it's value in the init method
+ *    to verify the same.
+ * 2. Make the global gvar64 declared above into a module param named 'val1'.
+ * 3. Allow it to be passed and set at both 
  *    a) insmod (or modprobe) time
- *    b) at any time (tip: use sysfs)
+ *    b) at any time by root (tip: use sysfs)
+ * 4. 
+ *    a) Arrange for the module to be auto-loaded at boot. Verify that it does.
+ *    b) Ensure it cannot be removed from memory, even by root.
  */
 
 /* Your code here */
