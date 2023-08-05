1cca93363755f1afea58899e2c95f8e141b3fad8
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-08-05 17:34:56 +0530

    initial commit

---
+++ helloworld_lkm/helloworld.c
@@ -0,0 +1,8 @@
+/* hello, world - kernel module */
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("Dual MIT/GPL");
+
+/* Your code here */
+
---
+++ helloworld_usermode/helloworld_nolibc/hello_no_libc.c
@@ -0,0 +1,12 @@
+/*
+ * Lab exercise: Write a C ‘Hello, world’ program without using any (g)libc routines (like printf() or puts())
+ * (c) Kaiwan NB, kaiwanTECH
+ * License: MIT
+ */
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+	/* Your code here */
+}
