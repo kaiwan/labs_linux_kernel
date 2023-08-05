440ea31bb2e3f8602da0ee87d4ed7e3d7152f7f4
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2023-08-05 18:00:51 +0530

    Add more todo's

--- helloworld_lkm/helloworld.c
+++ helloworld_lkm/helloworld.c
@@ -6,3 +6,19 @@ MODULE_LICENSE("Dual MIT/GPL");
 
 /* Your code here */
 
+/* 1. First do the simple 'Hello, world' output via the printk().
+ *    Also include the 'module author' and 'module description'.
+ * 
+ * ---Do the ones below only after the instructor has covered these topics---
+ *
+ * 2. Next, output 'Hello, world pr_info()' output via the pr_info() 
+ *
+ * 3. Output multiple 'Hello, world pr_*()' output via the pr_*() macros 
+ *
+ * 4. Change the Makefile symlink to point to the so-called 'better Makefile'
+ *    Use it:
+ *     - run various targets: help, indent, checkpatch, sa...
+ *    Study the output carefully and see if anything requires fixing.
+ *
+ */
+
